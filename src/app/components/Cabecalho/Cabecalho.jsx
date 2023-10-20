1b27cbd4844603d88fd38d739689c5bcfd1dd83a
filipeomendes <filipedeoliveiramendes@gmail.com>
2023-10-19 22:23:36 -0300

    Estilizando cabecalho e rodape com tailwind com layer e apply

--- src/app/components/Cabecalho/Cabecalho.jsx
+++ src/app/components/Cabecalho/Cabecalho.jsx
@@ -2,8 +2,10 @@ import Link from "next/link";
 
 export default function Cabecalho (){
     return (
-        <header>
-            <nav>
+        <header className="bg-slate-800 text-white">
+            <h1 className="text-center text-3xl font-extrabold">Produtos</h1>
+            <hr className="bg-red-600 border border-red-700"/>
+            <nav className="menu">
                 <ul>
                     <li>
                         <Link href="/produtos/tenis">Tênis</Link>
